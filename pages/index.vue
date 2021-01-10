d2cd4828b19b616fc355fbf251d82e1664acee4d
Batin Eryilmaz <batineryilmaz@icloud.com>
2021-01-10 03:43:32 +0300

    activate ssr & fix font issue on ssr

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,3 @@
-/*eslint no-throw-literal: 0*/
 <template>
   <div class="container">
     <div class="heading">
@@ -36,7 +35,7 @@
       :split-size="splitSize"
       :on-split-size-change="onSplitSizeChange"
     />
-    <EditButton />
+    <EditButton v-if="hasStarted" />
   </div>
 </template>
 
@@ -130,7 +129,14 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Reenie+Beanie&display=swap");
+@font-face {
+  font-family: 'Reenie Beanie';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url("../assets/ReenieBeanie-Regular.ttf") format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
 @keyframes slide-right-writer {
   from {
     left: -200px;
